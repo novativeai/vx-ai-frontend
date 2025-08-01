75077738ffb7ed12ed7aa90580e8f44b4ceef18d
novativeai <155618104+novativeai@users.noreply.github.com>
2025-08-01 17:52:40 +0300

    Update page.tsx

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -119,7 +119,7 @@ export default function SignIn() {
             </Button>
           </div>
           <div className="mt-4 text-center text-sm">
-            Don't have an account?{' '}
+            Don&apos;t have an account?{' '}
             <Link href="/signup" className="underline">
               Sign up
             </Link>
